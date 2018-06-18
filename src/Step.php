9246605c591bc06c1844234dee7d7b265546ee26
steve.brazier <steve.brazier@just-eat.com>
2018-06-18 20:53:21 +0100

    Add definition of a step

---
+++ src/Step.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MeadSteve\Tale;
+
+interface Step
+{
+    /**
+     * This function is called when all previous steps have succeeded.
+     *
+     * @param mixed $state The state passed in from the previous step
+     * @return mixed The state after completing this step
+     */
+    public function execute($state);
+
+    /**
+     * This function is called if a state later in the flow has failed. It
+     * is expected to reverse whatever was applied in execute
+     *
+     * @param mixed $state The state that was returned on this classes execute
+     */
+    public function compensate($state) : void;
+}
