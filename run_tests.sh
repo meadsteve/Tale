55e4fb64534962e93c778978630ff0cb9f08e439
steve.brazier <steve.brazier@just-eat.com>
2018-06-19 20:52:18 +0100

    reverse run_tests.sh order

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  set -ex
 
-./vendor/bin/phpcs --standard=PSR2 src/ tests/
-./vendor/bin/phpunit tests
 ./vendor/bin/phpstan analyse src --level max
+./vendor/bin/phpunit tests
+./vendor/bin/phpcs --standard=PSR2 src/ tests/
