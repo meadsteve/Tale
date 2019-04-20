32c5c6f507ca93e29f705fd5f74ac02b06e27153
steve <meadsteve@gmail.com>
2019-04-20 09:12:32 +0200

    CloneableState docs

--- src/State/CloneableState.php
+++ src/State/CloneableState.php
@@ -2,7 +2,21 @@
 
 namespace MeadSteve\Tale\State;
 
+/**
+ * Interface CloneableState
+ *
+ * How state is represented is up to the consumer of this library.
+ * If the state is an object and implements this interface then
+ * 'cloneState' will be called before passing the state into each state.
+ *
+ */
 interface CloneableState
 {
+    /**
+     * This method is expected to return a copy of the current object. How
+     * this is done is up to the implementer.
+     *
+     * @return mixed
+     */
     public function cloneState();
 }
